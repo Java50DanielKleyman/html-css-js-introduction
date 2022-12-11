c05e1882974e9e65d18326d616f0290ff7a1b43e
Daniel Kleyman <denny_klm1@yahoo.com>
2022-12-11 20:51:47 +0200

    HW-08 revisions

--- HW-08/src/presidents.js
+++ HW-08/src/presidents.js
@@ -1,14 +1,18 @@
 const detailsImage = document.querySelector(".details-image");
 const detailsTitle = document.querySelector(".details-title");
+const detailFont = document.querySelector(".details-title")
 const anchorElements = document.querySelectorAll(".thumbnails-anchor");
 function setDetails(anchor) {
     const dataImage = anchor.getAttribute("data-detailed-image");
     detailsImage.src = dataImage;
     detailsTitle.innerHTML = anchor.getAttribute("data-details-title");
+    detailFont.style.color = anchor.getAttribute("color");
 
 }
-for (let i=0;i<anchorElements.length; i++)
-{
-anchorElements[i].addEventListener("click",function(){setDetails(anchorElements[i])
-})
+for (let i = 0; i < anchorElements.length; i++) {
+    anchorElements[i].addEventListener("click", function () {
+        setDetails(anchorElements[i])
+    })
+
 }
+
