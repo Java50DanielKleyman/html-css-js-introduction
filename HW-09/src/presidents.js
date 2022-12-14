5e9111806663d7d0aa94f45d5f9910a63397207b
Daniel Kleyman <denny_klm1@yahoo.com>
2022-12-14 17:35:34 +0200

    HW-09 revisions

--- HW-09/src/presidents.js
+++ HW-09/src/presidents.js
@@ -2,9 +2,19 @@ const detailsImage = document.querySelector(".details-image");
 const detailsTitle = document.querySelector(".details-title");
 const detailFont = document.querySelector(".details-title")
 const anchorElements = document.querySelectorAll(".thumbnails-anchor");
+const mainElement = document.querySelector("main");
+const hideButtonElement = document.getElementById("hide-button");
+const HIDDEN = "hidden";
+function showDetails() {
+    mainElement.classList.remove(HIDDEN);
+}
+function hideDetails() {
+    mainElement.classList.add(HIDDEN);
+}
 function setDetails(anchor) {
     const dataImage = anchor.getAttribute("data-detailed-image");/* =anchor.dataset.detailsImage*/
     detailsImage.src = dataImage;
+    showDetails();
     detailsTitle.innerHTML = anchor.getAttribute("data-details-title");
     detailFont.style.color = "blue";
 
@@ -13,6 +23,5 @@ for (let i = 0; i < anchorElements.length; i++) {
     anchorElements[i].addEventListener("click", function () {
         setDetails(anchorElements[i])
     })
-
 }
-
+hideButtonElement.addEventListener("click", hideDetails);
