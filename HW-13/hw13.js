514416fd8767f157466868e2a47faa677fdb4bc5
Daniel Kleyman <denny_klm1@yahoo.com>
2022-12-29 13:42:11 +0200

    HW-13 revisions

--- HW-13/hw13.js
+++ HW-13/hw13.js
@@ -1,13 +1,16 @@
 function getHtmlUl(array) {
-    const res = [];
-    res[0] = '<ul class="list_class">';
-    res[array.length+1] = '</ul>'
-    for (let i = 1; i < array.length+1; i++) {
-        res[i] = array[i - 1] === 0 ? '<li class="item_class"><div class="white"></div></li>'
-            : '<li class="item_class"><div class="black"></div></li>';
+    const result = [];
+    let color = '';
+    result[0] = '<ul class="list_class">';
+    result[array.length + 1] = '</ul>'
+    for (let i = 1; i < array.length + 1; i++) {
+        color = array[i - 1] === 0 ? 'white' : 'black';
+        result[i] = `<li class="item_class"><div class="${color}"></div></li>`;
     }
-    return res.join('');
+    return result.join('');
 }
+console.log(getHtmlUl(getRandomArray(2, 0, 1)));
+
 function getRandomArray(length, min, max) {
     const myArray = [];
     for (let i = 0; i < length; i++) {
@@ -18,4 +21,31 @@ function getRandomArray(length, min, max) {
     return min + Math.trunc(Math.random() * (max - min + 1))
 }
 
-console.log(getHtmlUl(getRandomArray(2, 0, 1)));
+function transMatrix(matrix) {
+    let transMatrix = [];
+    let columns = matrix.length;
+    let rows = matrix[0].length;
+        for (let i = 0; i < rows; i++) {
+        transMatrix.push([])
+        for (let j = 0; j < columns; j++) {
+            let transitArray = matrix[j];
+            transMatrix[i].push(transitArray[i])
+        }
+            }
+    return transMatrix;
+}
+
+console.log(transMatrix(getMatrix(3, 2)));
+
+function getMatrix(rows, columns) {
+    const matrix = [];
+    let count = 1;
+    for (let i = 0; i < rows; i++) {
+        matrix.push([]);
+        for (let j = 0; j < columns; j++) {
+            matrix[i].push(count);
+            count++;
+        }
+    }
+    return matrix;
+}
