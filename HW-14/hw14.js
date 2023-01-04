a37970e192a1c475c187752fb49139dba27b18c4
Daniel Kleyman <denny_klm1@yahoo.com>
2023-01-04 21:51:46 +0200

    HW-14

---
+++ HW-14/hw14.js
@@ -0,0 +1,37 @@
+function coloringString(str1, str2) {
+    const array1 = Array.from(str1);
+    const array2 = Array.from(str2);
+    let arrayResult = [];
+    arrayResult = array2.map(function (number, index) {
+        let color = '';
+        const arrayTransit = array1.filter(function (number) {
+            if (number == array2[index])
+                return number;
+        })
+        if (array2[index] === array1[index])
+            color = 'green';
+        else if (array2[index] === arrayTransit[0])
+            color = 'yellow';
+        else color = 'red'
+        return color;
+    })
+    return arrayResult;
+}
+console.log(coloringString('pappy', 'apple'));
+
+function getNumbersWithDigitsAmount(digitsAmount, array) {
+    const resultArray = array.filter(function (number) {
+        let res = ''
+        if (number < 0) {
+            number = Math.abs(number).toString()
+            res = number.length === digitsAmount ? '-' + number : '';
+        }
+        else {
+            number = number.toString()
+            res = number.length === digitsAmount ? number : '';
+        }
+        return Number(res);
+    })
+    return resultArray;
+}
+console.log(getNumbersWithDigitsAmount(3, [1, 100, -100, 25, 1000]))
