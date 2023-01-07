b2337d2be5cbe9c889ca4716f98e69f3e672f703
Daniel Kleyman <denny_klm1@yahoo.com>
2023-01-07 21:36:34 +0200

    HW-15

---
+++ HW-15/hw15.js
@@ -0,0 +1,53 @@
+
+function evenOddSort(array) {
+    return array.sort(function (a, b) {
+        return Math.abs(a % 2) - Math.abs(b % 2);
+    })
+}
+console.log(`evens first from array [20, -10, 333, 1000, 552, 7, -7]: ${evenOddSort([20, -10, 333, 1000, 552, 7, -7])}`)
+
+function oddEvenSort(array) {
+    return array.sort(function (a, b) {
+        return Math.abs(b % 2) - Math.abs(a % 2);
+    })
+}
+console.log(`oddss first from array [20, -10, 333, 1000, 552, 7, -7]: ${oddEvenSort([20, -10, 333, 1000, 552, 7, -7])}`)
+
+function evenAscOddDesc(array) {
+    let arrayResult = evenOddSort(array).sort(function (a, b) {
+        if (a % 2 == 0 && b % 2 == 0)
+            return a - b;
+        else if (a % 2 != 0 && b % 2 == 0)
+            return 0;
+        return b - a;
+    });
+    return arrayResult;
+}
+console.log(`evens ascending odds descending [20, -10, 1001, 1000, 552, 7, -7]: ${evenAscOddDesc([20, -10, 1001, 1000, 552, 7, -7])}`)
+
+function getMin(array) {
+    return array.reduce(function (res, cur) {
+        return res = res > cur ? cur : res;;
+    })
+}
+console.log(`min from array [-1,15,-25,-100,12] = ${getMin([-1, 15, -25, -100, 12])}`)
+
+function getMax(array) {
+    return array.reduce(function (res, cur) {
+        return res = res < cur ? cur : res;;
+    })
+}
+console.log(`max from array [-1,15,-25,100,12] = ${getMax([-1, 15, -25, -100, 12])}`)
+
+function getAverage(array) {
+    return array.reduce(function (res, cur) {
+        return res + cur;
+    }) / array.length;
+
+}
+console.log(`average from array [2,4,3] = ${getAverage([2, 4, 3])}`)
+
+function getMinMaxAvg(array) {
+    return array = [[getMin(array)], [getMax(array)], [getAverage(array)]]
+}
+console.log(`MinMaxAvg from array [2,4,3] = ${getMinMaxAvg([2, 4, 3])}`)
