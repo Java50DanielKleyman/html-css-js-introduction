12ead8dc9f4e9598fa9c0e268246e9b8cf523e47
Daniel Kleyman <denny_klm1@yahoo.com>
2023-01-21 13:10:20 +0200

    A-18

--- HW-18/app.js
+++ HW-18/app.js
@@ -1,43 +1,30 @@
-function createEmployee(id, name, birthYear, salary, city, country) {
-    return { id, name, birthYear, salary, address: { city, country } }
+function createAddress(city, street) {
+    return { city, street }
 }
-const employees = [
-    createEmployee(123, "Vasya", 2000, 15000, "Lod", "Israel"),
-    createEmployee(124, "David", 1975, 15500, "Tel Aviv", "Israel"),
-    createEmployee(125, "Sara", 1985, 20000, "New York", "USA"),
-    createEmployee(126, "Abraham", 1990, 13000, "London", "UK"),
-    createEmployee(127, "Moshe", 2000, 15000, "Rehovot", "Israel"),
-    createEmployee(128, "Goga", 1993, 10000, "Tbilisi", "Gorgia"),
-    createEmployee(129, "Sasha", 2000, 25000, "Ramat Gan", "Israel"),
-    createEmployee(130, "Victor", 2003, 10000, "Rehovot", "Israel")
+function createPerson(id, name, address) {
+    return { id, name, address }
+}
+const persons = [
+    createPerson(123, "Vasya", createAddress("Rehovot", "Parshani")),
+    createPerson(124, "Olya", createAddress("Rehovot", "Pr.Plaut")),
+    createPerson(125, "Tolya", createAddress("Tel-Aviv", "Dizengoff")),
+    createPerson(126, "Sara", createAddress("Lod", "Sokolov"))
 ]
-function getEmployee(employees, id) {
-    return employees.find(employees => employees.id === id);
+function RehovotMaxId(persons, city) {
+    return persons.filter(person => person.address.city === city).sort((a, b) => b.id - a.id)[0].name
 }
-console.log(getEmployee(employees, 128));
+console.log(RehovotMaxId(persons, "Rehovot"))
+function statistics(persons) {
+    return persons.reduce((res, empl) => (empl.address.city === "Rehovot" ? res.Rehovot += 1 : 
+    empl.address.city === "Tel-Aviv" ? res['Tel-Aviv'] += 1 : res.Lod += 1) && res, 
+    { Rehovot: 0, 'Tel-Aviv': 0, Lod: 0 });
+}
 
-function getEmployeesBySalary(employees, salaryFrom, salaryTo) {
-    return employees.filter(employees => employees.salary >= salaryFrom 
-    && employees.salary <= salaryTo)
-}
-console.log(getEmployeesBySalary(employees, 10000, 12000))
+console.log(statistics(persons))
+//=======================================
+// 1 ) Calculate IN ONE LINE OF CODE the name of Person living in Rehovot and having maximal value of 'id'
+//     The expected result: Olya
 
-function getEmployeesByCity(employees, city) {
-    return employees.filter(employees => employees.address.city === city)    
-}
-console.log(getEmployeesByCity(employees, "Rehovot"))
-
-function getEmployeeNames(employees) {
-    return employees.map(employees => employees.name)
-}
-console.log(getEmployeeNames(employees));
-
-function sortEmployeesByAge(employees) {    
-    return employees.sort((a, b) => b.birthYear - a.birthYear);
-}
-console.log(sortEmployeesByAge(employees))
-
-function computeSalaryBudget(employees) {
-    return employees.reduce((a, b) => a + b.salary, 0)
-}
-console.log(computeSalaryBudget(employees));
+//=======================================
+// 2*) Build IN ONE LINE OF CODE the statistics of persons amount per city.
+//     The expected result is object: {Rehovot:2, 'Tel-Aviv':1,Lod:1}
