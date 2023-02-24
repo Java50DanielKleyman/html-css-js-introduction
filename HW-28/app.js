389c2174a43418b79833a07150526565cbce905b
Daniel Kleyman <denny_klm1@yahoo.com>
2023-02-24 09:16:18 +0200

    HW-28 rev

--- HW-28/app.js
+++ HW-28/app.js
@@ -1,18 +1,31 @@
+// class Deferred {
+//   str;
+//   constructor() {
+//     this.str = ''
+//   }
+//   resolve(word) {
+//     return this.str = word;
+//   }
+//   then(fun) {
+//     setTimeout(() => this.str = fun(this.str), 0)
+//   }
+// }
+
 class Deferred {
-    str;
-    constructor() {
-      this.str = ''
-    }
-    resolve(word) {
-      return this.str = word;
-    }
-    then(fun) {
-      setTimeout(() => this.str = fun(this.str), 0)
-    }
+  #functions;
+  constructor() {
+    this.#functions = []
   }
-  const d = new Deferred()
-  
-  d.then(function (res) { console.log("1 ", res); return "a"; });
-  d.then(function (res) { console.log("2 ", res); return "b"; });
-  d.then(function (res) { console.log("3 ", res); return "c"; });
-  d.resolve('hello');
+  then(fun) {
+    this.#functions.push(fun)
+  }
+  resolve(value) {
+    this.#functions.forEach(fun => value = fun(value))
+  }
+}
+const d = new Deferred()
+
+d.then(function (res) { console.log("1 ", res); return "a"; });
+d.then(function (res) { console.log("2 ", res); return "b"; });
+d.then(function (res) { console.log("3 ", res); return "c"; });
+d.resolve('hello');
