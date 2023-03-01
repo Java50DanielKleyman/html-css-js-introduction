be89fd12a7e9e89a675f060a2d167eced52eee85
Daniel Kleyman <denny_klm1@yahoo.com>
2023-03-01 19:04:43 +0200

    HW-30

--- HW-30/src/CipherDecipher.ts
+++ HW-30/src/CipherDecipher.ts
@@ -9,31 +9,29 @@ export class CipherImp implements Cipher {
         this.flag = flag;
     }
     cipher(plainText: string): string {
-        const arStr: Array<string> = Array.from(plainText);
+        if (!this.flag) {
+            return this.cipherDecipher(plainText, this.shift, this.mapperCipher)
+        }
+        return this.cipherDecipher(plainText, this.shift, this.mapperDecipher)
+    }
+    decipher(plainText: string): string {
+        if (!this.flag) {
+            return this.cipherDecipher(plainText, this.shift, this.mapperDecipher)
+        }
+        return this.cipherDecipher(plainText, this.shift, this.mapperCipher)
+    }
+    cipherDecipher(str: string, shift: number,
+        mapperFun: MapperFunction): string {
+        const arStr: Array<string> = Array.from(str);
         const arRes: Array<string> = arStr.map(symb => {
             let res: string = symb;
             if (symb <= '~' && symb >= ' ') {
-                if (!this.flag) {
-                    res = this.mapperCipher(symb, this.shift);
-                } else { res = this.mapperDecipher(symb, this.shift) };
+                res = mapperFun(symb, shift);
             }
             return res;
         })
         return arRes.join('');
     }
-    decipher(plainText: string): string {
-        const arStr: Array<string> = Array.from(plainText);
-        const arRes: Array<string> = arStr.map(symb => {
-            let res: string = symb;
-            if (symb <= '~' && symb >= ' ') {
-                if (!this.flag) {
-                    res = this.mapperDecipher(symb, this.shift);
-                } else { res = this.mapperCipher(symb, this.shift) };
-            }
-            return res;
-        })
-        return arRes.join('');
-    }    
     mapperCipher(symb: string, shift: number): string {
         const actualShift: number =
             (symb.charCodeAt(0) - startCodeAscii + shift) % nStrings;
--- HW-30/src/app.ts
+++ HW-30/src/app.ts
@@ -3,7 +3,7 @@ import { CipherRightShift } from "./CipherRightShift"
 import { displayCipherDecipher } from "./displayCipherDecipher"
 
 const cipherrightshift:  CipherRightShift = new CipherRightShift(2)
-const cipherleftshift: CipherLeftShift = new CipherLeftShift(1)
+const cipherleftshift: CipherLeftShift = new CipherLeftShift(3)
 displayCipherDecipher(cipherrightshift, 'aaa"')
 displayCipherDecipher(cipherleftshift, 'aaa"')
 
